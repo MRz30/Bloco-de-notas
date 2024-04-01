11ca47592e78d8d24f62952bdcac514d69e8ae61
MRz30 <matheus.rossini01@gmail.com>
2024-04-01 17:00:04 -0300

    Atualização do dia 01/04/2024

---
+++ js/num.py
@@ -0,0 +1,7 @@
+'''
+num1 = int(input ("Informe um número: "))
+num2 = int(input ("Informe outro número: "))
+num3 = num1 + num2
+print ("A soma dos dois número é: " , num3)
+'''
+
--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let modal = document.querySelector('#modal'); //Modal para edição das notas
 let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
 let notes = document.querySelector('#notes');//Lista divs com dados das notas
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-//let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
+let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
 
 /**
  * ===================== EVENTOS  =================================
@@ -35,15 +35,53 @@ btnSaveNote.addEventListener("click" , (evt) => {
         title: document.querySelector("#input-title").value,
         content: document.querySelector("#input-content").value
     }
-    console.log(data);
+    savenote(data);
 })
 
+btnCloseModal.addEventListener("click", (evt) =>{
+    evt.preventDefault();
+    modal.style.display = "none";
+    addNote.style.display = "block";
+    notes.style.display = "flex";
+    modalView.style.display = "none";
+    
+})
+
+
 /*
-================================== Função ==============================================
+================================== Funções ==============================================
 */
 
-const savenote = () => {
-    console.log(note);
+const savenote = (note) => {
+    
+    let notes = loadNotes();
+    note.lastTime = new Date().getTime();
+    if(note.id.trim().length < 1){
+        note.id = new Date().getTime();
+        notes.push(note);
+        document.querySelector('#input-id').value = note.id;
+
+    } 
+
+    else{
+
+        notes.forEach((item, i) =>{
+            if(item.id == note.id){
+                note[i] = note;
+            }
+        })
+    }
+
+        notes = JSON.stringify(notes);
+        
+        localStorage.setItem('notes', notes);
+
+
+        listNotes();
+
+};
+
+const loadNotes = () => {
     let notes = localStorage.getItem('notes');
 
     if(!notes){
@@ -53,24 +91,83 @@ const savenote = () => {
     else{
         notes = JSON.parse(notes);
     }
+    return notes;
+}
 
-    if(note.id.length < 1){
-        note.id = new Date().getTime();
-    } 
+const listNotes = () => {
+    notes.innerHTML = '';
+    let listNotes = loadNotes();
+    listNotes.forEach((note) => {
+        let divCard = document.createElement('div');
+        divCard.className = 'card';
+        divCard.style.width = '25rem';
+        let divCardBody = document.createElement('div');
+        divCardBody.className = 'card-body'
+        divCard.appendChild(divCardBody);
+        let h5 = document.createElement('h5')
+        h5.innerText = note.title;
+        divCardBody.appendChild(h5);
+        let pContent = document.createElement('p');
+        pContent.innerText = note.content;
+        let pLastTime = document.createElement('p');
+        pLastTime.innerText = "Atualizado em: "+dateFormat(note.lastTime);
+        divCardBody.appendChild(pContent);
+        divCardBody.appendChild(pLastTime);
 
-    else{
+        notes.appendChild(divCard);
 
-    }
+        notes.addEventListener("click" , (evt) => {
+            evt.preventDefault ();
+            showNote(note);
+        
+        })
+    });
+}
 
-    note.lastTime = new Date().getTime();
+const showNote = (note)=>{
+    document.querySelector('#controls-note').innerHTML = "";
+    notes.style.display = "none";
+    modalView.style.display = "block";
+    addNote.style.display ="none";
+    document.querySelector('#title-note').innerHTML= "<h1>"+note.title+"</h1>";
+    document.querySelector('#content-note').innerHTML = `<p>${note.content}</p>
+    <p>Ultima alteração: ${dateFormat(note.lastTime)}</p>`
+    let divEdit = document.createElement("div");
+    let iEdit = document.createElement("i");
+    iEdit.className = "bi bi-pen";
+    divEdit.appendChild(iEdit);
+    document.querySelector("#controls-note").appendChild(divEdit);
+    divEdit.addEventListener ("click", (evt) => {
+        evt.preventDefault();
+        editNote(note);
+        
+        
 
-    notes.push(note);
+    }) 
+
+    let divRemote = document.createElement("div");
+    let iRemote = document.createElement("IR");
+    iRemote.className = 'bi bi-trash3';
+    divRemote.appendChild(iRemote);
+    document.querySelector("#controls-note").appendChild(divRemote);
     
-    notes = JSON.stringify(notes);
+};
 
-    localStorage.setItem('notes', notes);
+const editNote = (note) => {
+    
+    document.querySelector("#content-note").value = note.content;
+    document.querySelector("#title-note").value = note.title;
+    modalView.style.display = "none";
+    modal.style.display = "block";
+}
 
 
+
+const dateFormat = (timestamp) => {
+    let date = new Date(timestamp);
+    date = date.toLocaleDateString("pt-BR");
+    return date;
 };
 
 
+listNotes();
